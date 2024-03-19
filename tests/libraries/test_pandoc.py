3d3394080ffea17fb38af8882d310a2276457eb1
daizutabi <daizutabi@gmail.com>
2024-03-19 20:09:45 +0900

    Delete test for pandoc 3.1.12

--- tests/libraries/test_pandoc.py
+++
@@ -1,6 +0,0 @@
-from panflute.tools import pandoc_version
-
-
-def test_pandoc():
-    v = str(pandoc_version)
-    assert v.split(".")[:2] == ["3", "1"]
--- tests/libraries/test_span.py
+++
@@ -1,9 +0,0 @@
-import panflute as pf
-from panflute import Math, Plain, Span
-
-
-def test_span():
-    s = Span(Math("x=1"), identifier="x", classes=["c"], attributes={"k": "v"})
-    tex = pf.convert_text(Plain(s), input_format="panflute", output_format="latex")
-    assert isinstance(tex, str)
-    assert tex == "\\phantomsection\\label{x}{\\[x=1\\]}"
