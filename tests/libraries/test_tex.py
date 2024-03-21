9cfd113376545cf41ed9fbb5bd55ea5128150d19
daizutabi <daizutabi@gmail.com>
2024-03-21 20:27:15 +0900

    tex command escape

--- tests/libraries/test_tex.py
+++ tests/libraries/test_tex.py
@@ -1,16 +1,32 @@
 import panflute as pf
-from panflute import Para, RawInline, Str
+import pytest
+from panflute import Para, RawInline, Space, Str
 
 
-def test_tex():
-    text = "\\noindent abc"
+@pytest.mark.parametrize("x", ["abc", "[@ref]"])
+@pytest.mark.parametrize("sep", ["", "{}"])
+def test_tex(x, sep):
+    text = f"\\noindent{sep} {x}"
     elems = pf.convert_text(text)
     assert isinstance(elems, list)
     para = elems[0]
     assert isinstance(para, Para)
     raw = para.content[0]
     assert isinstance(raw, RawInline)
-    assert raw.text == "\\noindent "
-    s = para.content[1]
-    assert isinstance(s, Str)
-    assert s.text == "abc"
+    if x == "abc":
+        if sep:
+            assert raw.text == f"\\noindent{sep}"
+        else:
+            assert raw.text == "\\noindent "
+        s = para.content[1]
+        if sep:
+            assert isinstance(s, Space)
+        else:
+            assert isinstance(s, Str)
+            assert s.text == "abc"
+    elif sep:
+        assert raw.text == f"\\noindent{sep}"
+        s = para.content[1]
+        assert isinstance(s, Space)
+    else:
+        assert raw.text == text
