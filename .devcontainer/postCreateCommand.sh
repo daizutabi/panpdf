dc8a572af81aa7df3065864c80e9e31d66a88f5f
daizutabi <daizutabi@gmail.com>
2024-03-22 15:43:52 +0900

    Revert devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 sudo chown -R vscode:vscode .
-# pipx install hatch
-# hatch config set dirs.env.virtual .hatch
-# curl -sS https://starship.rs/install.sh | sh -s -- --yes
-# echo 'eval "$(starship init bash)"' >> ~/.bashrc
-# echo 'alias lualatex "sudo docker run -i --rm "' >> ~/.bashrc
+pipx install hatch
+hatch config set dirs.env.virtual .hatch
+curl -sS https://starship.rs/install.sh | sh -s -- --yes
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
