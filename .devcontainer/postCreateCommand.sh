c4183da44a5d2a8cdb56a4bf5c8ebd40d8e2823b
daizutabi <daizutabi@gmail.com>
2024-03-23 17:14:42 +0900

    Use texlive image

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-sudo chown -R vscode:vscode .
 hatch config set dirs.env.virtual .hatch
-curl -sS https://starship.rs/install.sh | sh -s -- --yes
-echo 'eval "$(starship init bash)"' >> ~/.bashrc
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
