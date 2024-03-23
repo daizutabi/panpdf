747c8846e9c37366fcc710e1359092b7329696b1
daizutabi <daizutabi@gmail.com>
2024-03-23 13:18:48 +0900

    vscode extension

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 sudo chown -R vscode:vscode .
-pipx install hatch
 hatch config set dirs.env.virtual .hatch
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
