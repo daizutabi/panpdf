b5287eda1b991fcd9b76404280c0ff866cda13cb
Daizu <daizutabi@gmail.com>
2024-03-28 17:35:33 +0900

    Remove unused functions

--- tests/filters/test_verbatim.py
+++ tests/filters/test_verbatim.py
@@ -1,16 +1,7 @@
 import panflute as pf
 from panflute import Doc
 
-# from panpdf.filters.outputcell import OutputCell
 
-
-# def test_outputcell_fenced_code():
-#     text = "```julia\na = 1\n```\n\n```output\n1\n```\n"
-#     code = pf.convert_text(text)[1]  # type:ignore
-#     code = OutputCell().action(code, None)  # type:ignore
-#     tex = pf.convert_text(code, input_format="panflute", output_format="latex")
-#     assert "\\vspace{-0.5\\baselineskip}\\definecolor" in tex  # type:ignore
-#     assert "\\definecolor{shadecolor}{RGB}{240,240,250}" in tex  # type:ignore
 def test_create_header():
     from panpdf.filters.verbatim import create_header
 
