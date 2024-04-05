f974d2f3ec1b77e902d20002016dafe352d80209
Daizu <daizutabi@gmail.com>
2024-04-05 17:14:37 +0900

    Add title

--- src/panpdf/filters/verbatim.py
+++ src/panpdf/filters/verbatim.py
@@ -10,11 +10,10 @@ from panpdf.filters.filter import Filter
 from panpdf.tools import add_metadata_list, create_temp_file
 
 if TYPE_CHECKING:
+    from collections.abc import Iterable
     from pathlib import Path
 
     from panflute import Doc, Element
-
-DEFAULT_SHADE_COLOR = "\\definecolor{shadecolor}{RGB}{240,240,250}"
 
 
 @dataclass(repr=False)
@@ -25,13 +24,15 @@ class Verbatim(Filter):
         self.shaded = False
 
     def action(self, elem: CodeBlock, doc: Doc) -> CodeBlock | list[Element]:  # noqa: ARG002
-        if "output" not in elem.classes:
-            return elem
-
         self.shaded = True
-        elem.classes.pop(elem.classes.index("output"))
-        pre = "\\vspace{-0.5\\baselineskip}\\definecolor{shadecolor}{rgb}{1,1,0.9}%"
-        return [RawBlock(pre, format="latex"), elem, RawBlock(DEFAULT_SHADE_COLOR, format="latex")]
+
+        if "output" in elem.classes:
+            return create_output(elem)
+
+        if "title" in elem.attributes:
+            return create_title(elem)
+
+        return create_default(elem)
 
     def finalize(self, doc: Doc) -> None:
         if self.shaded:
@@ -39,16 +40,64 @@ class Verbatim(Filter):
             add_metadata_list(doc, "include-in-header", path.as_posix())
 
 
-def create_header() -> Path:
-    text = r"""
-    \ifdefined\Shaded
-    \usepackage{framed}
-    \renewenvironment{Shaded}{\begin{quotation}\begin{snugshade}\linespread{0.9}}%
-    {\end{snugshade}\end{quotation}}
-    \definecolor{shadecolor}{RGB}{240,240,250}
-    \DefineVerbatimEnvironment{Highlighting}{Verbatim}%
-    {commandchars=\\\{\},fontsize=\small,numbers=left}
-    """
-    text = inspect.cleandoc(text)
-    text = f"{text}\n{DEFAULT_SHADE_COLOR}\n\\fi"
+def create_output(code: CodeBlock) -> list[Element]:
+    code.classes.pop(code.classes.index("output"))
+
+    return [vspace(), *create_code_block(code, OUTPUT_SHADE_COLOR)]
+
+
+def create_title(code: CodeBlock) -> list[Element]:
+    attrs = {"formatcom": f"\\color{{{TITLE_COLOR}}}\\bfseries"}
+    title = CodeBlock(code.attributes.pop("title"), attributes=attrs)
+
+    return [
+        *create_code_block(title, TITLE_SHADE_COLOR),
+        vspace(),
+        *create_code_block(code, DEFAULT_SHADE_COLOR),
+    ]
+
+
+def create_default(code: CodeBlock) -> list[Element]:
+    return create_code_block(code, DEFAULT_SHADE_COLOR)
+
+
+def create_code_block(code: CodeBlock, rgb: Iterable) -> list[Element]:
+    env = define_verbatim_environment(code.attributes)
+    color = define_shade_color(rgb)
+
+    if not code.classes:
+        code.classes = ["text"]
+
+    return [RawBlock(f"{env}{color}", format="latex"), code]
+
+
+def vspace() -> RawBlock:
+    return RawBlock("\\vspace{-0.5\\baselineskip}", format="latex")
+
+
+TITLE_SHADE_COLOR = (0.9, 0.9, 1)
+DEFAULT_SHADE_COLOR = (0.94, 0.94, 0.94)
+OUTPUT_SHADE_COLOR = (1, 1, 0.9)
+TITLE_COLOR = "NavyBlue"
+
+
+def define_verbatim_environment(attrs: dict[str, str], fontsize: str = "small") -> str:
+    text = r"\DefineVerbatimEnvironment{Highlighting}{Verbatim}{commandchars=\\\{\},"
+    text = f"{text}fontsize=\\{fontsize}"
+    args = ",".join(f"{k}={v}" for k, v in attrs.items())
+    return f"{text},{args}}}" if args else text + "}"
+
+
+def define_shade_color(rgb: Iterable) -> str:
+    rgb_str = ",".join(str(x) for x in rgb)
+    return f"\\definecolor{{shadecolor}}{{rgb}}{{{rgb_str}}}"
+
+
+def create_header(linespread: float = 0.9) -> Path:
+    text = (
+        "\\ifdefined\\Shaded\\usepackage{framed}"
+        "\\renewenvironment{Shaded}{\\begin{quotation}\\begin{snugshade}\\linespread{"
+        f"{linespread}"
+        "}}{\\end{snugshade}\\end{quotation}}\\fi"
+    )
     return create_temp_file(text, suffix=".tex")
