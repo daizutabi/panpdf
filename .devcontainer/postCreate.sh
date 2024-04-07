9bcca00e5321eaf4feca7e153505b36f0aa94b3d
Daizu <daizutabi@gmail.com>
2024-04-07 22:45:13 +0000

    Add alias

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -2,5 +2,6 @@
 
 hatch config set dirs.env.virtual .hatch
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
+echo "alias ll='ls -alF'" >> ~/.bashrc
 mkdir -p ~/.config
 cp .devcontainer/starship.toml ~/.config
