1945905073650340f710d1f668c57702a7d2c659
Daizu <daizutabi@gmail.com>
2024-04-20 22:49:16 +0000

    Font settings

--- .devcontainer/features/zotero/install.sh
+++ .devcontainer/features/zotero/install.sh
@@ -2,9 +2,9 @@
 set -e
 
 apt-get update
-apt-get install -y sudo
+apt-get install -y --no-install-recommends sudo
 curl -sL https://raw.githubusercontent.com/retorquere/zotero-deb/master/install.sh | sudo bash
 apt-get update
-apt-get install -y zotero
+apt-get install -y --no-install-recommends zotero
 apt-get clean
 rm -rf /var/lib/apt/lists/*
